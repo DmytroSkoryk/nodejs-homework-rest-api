dd580a009b095469909f127384f787afc28291c6
DmytroSkoryk <114485103+DmytroSkoryk@users.noreply.github.com>
2023-07-18 19:51:53 +0300

    first attempt

---
+++ models/user.js
@@ -0,0 +1,64 @@
+const Joi = require("joi");
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers");
+
+const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      match: emailPattern,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailPattern).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailPattern).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const subscriptionSchema = Joi.object({
+  subscription: Joi.string().required(),
+});
+
+const User = model("user", userSchema);
+const schemas = {
+  registerSchema,
+  loginSchema,
+  subscriptionSchema,
+};
+
+module.exports = {
+  User,
+  schemas,
+};
